a520513246527636d56274ba8f234cdd33c5289c
Kiryl Pashkin <sleembelbox@gmail.com>
2023-08-12 16:50:51 +0300

    add hw5

--- homeworks/hw2/models.py
+++ homeworks/hw2/models.py
@@ -8,8 +8,7 @@ class Client(models.Model):
     address = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'Client info:\nName: {self.name}\nEmail: {self.email}\nPhone number: {self.phone}\n' \
-               f'Address: {self.address}\n'
+        return self.name
 
 
 class Product(models.Model):
@@ -21,8 +20,7 @@ class Product(models.Model):
     image = models.ImageField()
 
     def __str__(self):
-        return f'Product info:\nName: {self.name}\nDescription: {self.description}\nPrice: {self.price}\n' \
-               f'Pieces: {self.quantity}\nCreate date: {self.date_of_order}\n'
+        return self.name
 
 
 class Order(models.Model):
@@ -30,6 +28,9 @@ class Order(models.Model):
     products = models.ManyToManyField(Product)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     date_of_order = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Order of client: {self.client}'
 
     
 
